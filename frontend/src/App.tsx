a36d260e237c183716cb5c2300ee2836103f5a9a
Claudio Oliveira <c.rosa.oliveira@gmail.com>
2022-01-11 22:05:33 -0300

    Pagination Layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,10 +7,12 @@ import Listing from 'pages/Listing';
 import Form from 'pages/Form';
 import Navbar from "components/Navbar";
 
+
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
+      
       <Routes>
         <Route path="/" element={<Listing />} />
         <Route path="/form">
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,8 @@
+import Pagination from "components/Pagination";
 function Listing(){
 
     return(
-        <h1>Listagem</h1>
+        <Pagination />
     )
 }
 export default Listing;
